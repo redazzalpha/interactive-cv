43d851da361838338e13208c201f4642579e70f8
redazzalpha <redazzalpha@gmail.com>
2024-01-20 09:18:51 +0100

    add base avatar on drawer

--- src/layouts/default/Drawer.vue
+++ src/layouts/default/Drawer.vue
@@ -1,3 +1,9 @@
 <template>
-  <v-navigation-drawer></v-navigation-drawer>
+  <v-navigation-drawer>
+    <v-list class="mt-10">
+      <v-list-item class="d-flex justify-center">
+        <v-avatar size="170" image="@/assets/wolf.jpg"></v-avatar>
+      </v-list-item>
+    </v-list>
+  </v-navigation-drawer>
 </template>

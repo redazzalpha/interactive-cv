3b2c260b2f4f15d25666d8b693ef6aee0c63e463
redazzalpha <redazzalpha@gmail.com>
2024-01-21 17:34:14 +0100

    fix drawer nav  open/close

--- src/layouts/default/DrawerNav.vue
+++ src/layouts/default/DrawerNav.vue
@@ -1,12 +1,20 @@
 <template>
-  <v-navigation-drawer
+  <AppDrawer
     location="right"
-    disable-resize-watcher
-    :model-value="useAppStore().drawer"
-  >
-  </v-navigation-drawer>
+    :disable-resizer="true"
+    v-model="store.drawer"
+  ></AppDrawer>
 </template>
 
 <script setup lang="ts">
+import vuetify from "@/plugins/vuetify";
 import { useAppStore } from "@/store/app";
+const store = useAppStore();
+
+//#region event handler
+function onResize(): void {
+  if (!vuetify.display.mobile.value) store.setDrawer(false);
+}
+document.body.onresize = onResize;
+//#endregion
 </script>
